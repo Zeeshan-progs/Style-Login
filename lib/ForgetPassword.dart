3fa925c5e16fd63e8c8b6995ce265022f911e050
zeeshan_progs <md.zeeshaniqbal7277@gmail.com>
2020-08-10 14:33:47 +0530

    Adding Register Page

--- lib/ForgetPassword.dart
+++ lib/ForgetPassword.dart
@@ -1,23 +1,172 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ForgetPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Center(
           child: Text("Forget Password"),
         ),
       ),
-      backgroundColor: Colors.amber,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            "Wait some Time Will Update Soon",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 50,
+          Container(
+            height: size.height / 1.8,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.indigo[900],
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(70),
+                bottomLeft: Radius.circular(70),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.4),
+                  blurRadius: 20,
+                ),
+              ],
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 60),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  height: 50,
+                  width: size.width / 1.2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.4),
+                        blurRadius: 20,
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Confirm Email",
+                      icon: Icon(Icons.alternate_email),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  width: size.width / 1.2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(.5), blurRadius: 20),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Old Password",
+                      icon: Icon(Icons.vpn_key),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  width: size.width / 1.2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(.5), blurRadius: 20),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "New Password",
+                      icon: Icon(Icons.vpn_key),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  width: size.width / 1.2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(.5), blurRadius: 20),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Confirm Password",
+                      icon: Icon(Icons.vpn_key),
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        color: Colors.blueGrey,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(40),
+                          ),
+                        ),
+                        child: Text(
+                          "Confirm",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                      SizedBox(width: 60),
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        color: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(40),
+                          ),
+                        ),
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
